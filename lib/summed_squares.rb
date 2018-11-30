573bf4451d10c569d09addd16d64548de65cbfc4
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-11-30 09:57:14 -0700

    Adds and tests method to determine square root

---
+++ lib/summed_squares.rb
@@ -0,0 +1,17 @@
+def list_squared(range_start, range_end)
+  desired_range = ("#{range_start}".."#{range_end}").to_a
+end
+
+def check_sum_of_each(number)
+  range = (1..number).to_a
+  range.reduce([]) do |array, num|
+    if number % num.to_i == 0
+      array << num
+    end
+    array.map {|divisor| divisor * 2}
+  end.sum
+end
+
+def is_square?(number)
+  (Math.sqrt(number) % 1).zero?
+end
--- test/summed_squares_test.rb
+++ test/summed_squares_test.rb
@@ -1,8 +1,29 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/summedsquares'
+require './lib/summed_squares'
 require 'pry'
 
 class SummedSquaresTest < Minitest::Test
-  
+  def test_returns_array_of_correct_range
+    number_1 = 1
+    number_2 = 5
+    range = list_squared(number_1, number_2)
+    assert_equal 5, range.length
+  end
+
+  def test_returns_even_divisors_from_one_to_target_number
+    number = 5
+    result = [1]
+    abridged = check_sum_of_each(number)
+    assert_equal result, abridged
+  end
+
+  def test_it_can_determine_if_a_number_is_square
+    number_1 = 4
+    number_2 = 5
+    result_1 = true
+    result_2 = false
+    assert_equal result_1, is_square?(number_1)
+    assert_equal result_2, is_square?(number_2)
+  end
 end
