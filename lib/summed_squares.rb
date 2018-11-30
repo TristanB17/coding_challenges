666b93e1717e0fe85e6ae9cfdec8b62f4b7c7661
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-11-30 12:33:30 -0700

    Changes approach to problem

--- lib/summed_squares.rb
+++ lib/summed_squares.rb
@@ -1,14 +1,18 @@
 def list_squared(range_start, range_end)
-  desired_range = ("#{range_start}".."#{range_end}").to_a
+  desired_range = (range_start..range_end).to_a
+end
+
+def list_achieved(range_start, range_end)
+  desired_range = (range_start..range_end).to_a
 end
 
 def check_sum_of_each(number)
   range = (1..number).to_a
   range.reduce([]) do |array, num|
-    if number % num.to_i == 0
+    if (number % num).integer?
       array << num
     end
-    array.map {|divisor| divisor * 2}
+    array.map { |divisor| divisor * 2 }
   end.sum
 end
 
--- test/summed_squares_test.rb
+++ test/summed_squares_test.rb
@@ -4,19 +4,19 @@ require './lib/summed_squares'
 require 'pry'
 
 class SummedSquaresTest < Minitest::Test
-  def test_returns_array_of_correct_range
-    number_1 = 1
-    number_2 = 5
-    range = list_squared(number_1, number_2)
-    assert_equal 5, range.length
-  end
-
-  def test_returns_even_divisors_from_one_to_target_number
-    number = 5
-    result = [1]
-    abridged = check_sum_of_each(number)
-    assert_equal result, abridged
-  end
+  # def test_returns_array_of_correct_range
+  #   number_1 = 1
+  #   number_2 = 5
+  #   range = list_squared(number_1, number_2)
+  #   assert_equal 5, range.length
+  # end
+  #
+  # def test_returns_even_divisors_from_one_to_target_number
+  #   number = 5
+  #   result = [1]
+  #   abridged = check_sum_of_each(number)
+  #   assert_equal result, abridged
+  # end
 
   def test_it_can_determine_if_a_number_is_square
     number_1 = 4
