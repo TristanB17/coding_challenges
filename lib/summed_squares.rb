ac63f60d1823c4a03569774876322bc710c20c1a
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-12-06 15:14:49 -0700

    Restructuring tests and methods to break process into smaller components

--- lib/summed_squares.rb
+++ lib/summed_squares.rb
@@ -1,21 +1,33 @@
-def list_squared(range_start, range_end)
-  desired_range = (range_start..range_end).to_a
-end
 
-def list_achieved(range_start, range_end)
-  desired_range = (range_start..range_end).to_a
-end
-
-def check_sum_of_each(number)
-  range = (1..number).to_a
-  range.reduce([]) do |array, num|
-    if (number % num).integer?
-      array << num
+  def list_squared(range_start, range_end)
+    desired_range = get_range(range_start, range_end)
+    desired_range.reduce([]) do |array, number|
+      sum_of_divisors_squared = check_sum_of_each(number)
+      if is_square?(sum_of_divisors_squared)
+      end
     end
-    array.map { |divisor| divisor * 2 }
-  end.sum
-end
+  end
 
-def is_square?(number)
-  (Math.sqrt(number) % 1).zero?
-end
+  def get_range(range_start, range_end)
+    (range_start..range_end).to_a
+  end
+
+  def check_sum_of_each(number)
+    range = (1..number).to_a
+    range.reduce([]) do |array, num|
+      if (number % num).integer?
+        array << num
+      end
+      array.map { |divisor| divisor * 2 }
+    end.sum
+  end
+
+  def is_square?(number)
+    (Math.sqrt(number) % 1).zero?
+  end
+
+#   Divisors of 42 are : 1, 2, 3, 6, 7, 14, 21, 42. These divisors squared are: 1, 4, 9, 36, 49, 196, 441, 1764. The sum of the squared divisors is 2500 which is 50 * 50, a square!
+#
+# Given two integers m, n (1 <= m <= n) we want to find all integers between m and n whose sum of squared divisors is itself a square. 42 is such a number.
+#
+# The result will be an array of arrays or of tuples (in C an array of Pair) or a string, each subarray having two elements, first the number whose squared divisors is a square and then the sum of the squared divisors.
--- test/summed_squares_test.rb
+++ test/summed_squares_test.rb
@@ -4,19 +4,21 @@ require './lib/summed_squares'
 require 'pry'
 
 class SummedSquaresTest < Minitest::Test
-  # def test_returns_array_of_correct_range
-  #   number_1 = 1
-  #   number_2 = 5
-  #   range = list_squared(number_1, number_2)
-  #   assert_equal 5, range.length
-  # end
-  #
-  # def test_returns_even_divisors_from_one_to_target_number
-  #   number = 5
-  #   result = [1]
-  #   abridged = check_sum_of_each(number)
-  #   assert_equal result, abridged
-  # end
+  def test_returns_array_of_correct_range
+    number_1 = 1
+    number_2 = 5
+    array = [1, 2, 3, 4, 5]
+    range = get_range(number_1, number_2)
+    assert_equal array, range
+    assert_equal 5, range.length
+  end
+
+  def test_adds_divisors_squared_from_one_to_target_number
+    number = 5
+    result = [1]
+    abridged = check_sum_of_each(number)
+    assert_equal result, abridged
+  end
 
   def test_it_can_determine_if_a_number_is_square
     number_1 = 4
@@ -26,4 +28,10 @@ class SummedSquaresTest < Minitest::Test
     assert_equal result_1, is_square?(number_1)
     assert_equal result_2, is_square?(number_2)
   end
+
+  # def test_it_squares_and_adds_numbers
+  #   sample_array = [1, 2, 3, 6, 7, 14, 21, 42]
+  #   result = true
+  #   assert_equal result, sample_array
+  # end
 end
