7bc4b9720a1189a8a014ff509fc77227eb9829a8
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-12-06 20:36:24 -0700

    Completes MVP for the wave

--- lib/the_wave.rb
+++ lib/the_wave.rb
@@ -1,7 +1,24 @@
-def get_alphabet
-  Hash[('a'..'z').zip('A'..'Z')]
-end
+class TheWave
+  attr_reader :alphabet
 
-def do_the_wave(word)
-  
+  def initialize
+    @alphabet = get_alphabet
+  end
+
+  def get_alphabet
+    Hash[('a'..'z').zip('A'..'Z')]
+  end
+
+  def do_the_wave(word)
+    word.chars.map.with_index do |letter, index|
+      duplicate = word.dup
+      replace_a_letter(duplicate, letter, index) if alphabet[letter]
+    end
+  end
+
+  def replace_a_letter(word, letter, index)
+    word.insert(index, letter.capitalize)
+    word.slice!(index + 1)
+    word
+  end
 end
--- test/the_wave_test.rb
+++ test/the_wave_test.rb
@@ -5,14 +5,33 @@ require 'pry'
 
 class TheWaveTest < Minitest::Test
   def test_can_capitalize_string
+    new_wave = TheWave.new
     word = 'howdy'
     expected = ['Howdy', 'hOwdy', 'hoWdy', 'howDy', 'howdY']
+    result = new_wave.do_the_wave(word)
+    assert_equal expected, result
   end
 
   def test_creates_alphabet_hash_object
     expected = 26
-    result = get_alphabet
+    result = TheWave.new.get_alphabet
     assert_equal Hash, result.class
     assert_equal expected, result.length
+  end
+
+  def test_it_initializes_with_alphabet
+    new_wave = TheWave.new
+    letters_in_the_alphabet = 26
+    assert_equal letters_in_the_alphabet, new_wave.alphabet.length
+  end
+
+  def test_returns_version_of_word_with_capitalized_letter
+    letter = 'a'
+    index = 1
+    word = 'barnacle'
+    modified_word = 'bArnacle'
+    new_wave = TheWave.new
+    result = new_wave.replace_a_letter(word, letter, index)
+    assert_equal modified_word, result
   end
 end
