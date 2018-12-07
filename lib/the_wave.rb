4cc8a3ce67160765b172c32a5fc7c9a8b1c1b6b7
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-12-06 20:38:19 -0700

    Removes class aspect of code challenge, reduced to only methods for simplicity

--- lib/the_wave.rb
+++ lib/the_wave.rb
@@ -1,24 +1,17 @@
-class TheWave
-  attr_reader :alphabet
+def get_alphabet
+  Hash[('a'..'z').zip('A'..'Z')]
+end
 
-  def initialize
-    @alphabet = get_alphabet
+def do_the_wave(word)
+  alphabet = get_alphabet
+  word.chars.map.with_index do |letter, index|
+    duplicate = word.dup
+    replace_a_letter(duplicate, letter, index) if alphabet[letter]
   end
+end
 
-  def get_alphabet
-    Hash[('a'..'z').zip('A'..'Z')]
-  end
-
-  def do_the_wave(word)
-    word.chars.map.with_index do |letter, index|
-      duplicate = word.dup
-      replace_a_letter(duplicate, letter, index) if alphabet[letter]
-    end
-  end
-
-  def replace_a_letter(word, letter, index)
-    word.insert(index, letter.capitalize)
-    word.slice!(index + 1)
-    word
-  end
+def replace_a_letter(word, letter, index)
+  word.insert(index, letter.capitalize)
+  word.slice!(index + 1)
+  word
 end
